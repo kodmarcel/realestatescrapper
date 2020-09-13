20c286ba63d8eea141ae5170e044af768216c246
Marcel Salmic <salmic.marcel@gmail.com>
2020-09-13 21:29:59 +0200

    Updated example

--- example_rent.py
+++ example_rent.py
@@ -1,48 +1,55 @@
-from run import main
+from run import main, analyze_data
 
 name = "example_rent"
-scrap_urls = ["https://www.nepremicnine.net/oglasi-oddaja/ljubljana-mesto/stanovanje/cena-do-450-eur-na-mesec/"]
+scrap_urls = ["https://www.nepremicnine.net/oglasi-oddaja/ljubljana-mesto/stanovanje/garsonjera,1-sobno,1.5-sobno/cena-do-500-eur-na-mesec/"]
 distance_from = "Kongresni trg, Ljubljana, Slovenija"
 ignore_list = ["oddamo sob","oddam sob","oddaja se sob", "oddajam sobo", "študentsko sobo", "oddaja se postelj","oddaja postelj","oddamo postelj", "oddamo dvoposteljno", "souporab","delit", "skupno", "že stanuje", "oddamo posteljo", "postelji oddamo", "https://www.nepremicnine.net/oglasi-oddaja/lj-siska-stanovanje_6360581/", "https://www.nepremicnine.net/oglasi-oddaja/jezica-bezigrad-ruski-car-stanovanje_6359280/", "https://www.nepremicnine.net/oglasi-oddaja/lj-vic-sibeniska-ulica-5-stanovanje_6309122/", "https://www.nepremicnine.net/oglasi-oddaja/lj-vic-rozna-dolina-stanovanje_6203756/", "https://www.nepremicnine.net/oglasi-oddaja/lj-moste-stanovanje_6323521/","https://www.nepremicnine.net/oglasi-oddaja/lj-vic-stanovanje_6359292/"]
 scrape_file = "scraped_data/" + name + ".csv"
 archive_data_file = "archive_data/" + name + ".csv"
-print_columns = ["points", "location", "price", "size", "distance", "new", "url"]
+print_columns = ["points", "location", "price", "size", "distance", "captured_today", "url"]
 
 
 def calculate_points(estate):
     if not estate["active"]:
         return 0
-    if estate['size'] < 10:
-        size_points = 0
-    elif estate['size'] < 20:
-        size_points = 30
-    elif estate['size'] < 30:
-        size_points = 60
-    elif estate['size'] < 50:
-        size_points = 70
-    elif estate['size'] < 60:
-        size_points = 80
+    if not estate["captured_today"]:
+        old = -50
     else:
-        size_points = 0
+        old = 0
+
+    # if estate['size'] < 10:
+    #     size_points = 0
+    # elif estate['size'] < 20:
+    #     size_points = 30
+    # elif estate['size'] < 30:
+    #     size_points = 60
+    # elif estate['size'] < 50:
+    #     size_points = 70
+    # elif estate['size'] < 60:
+    #     size_points = 80
+    # else:
+    #     size_points = 0
 
     if estate["price"] < 100:
-        price_points = 10
-    elif estate["price"] < 150:
         price_points = 100
+    elif estate["price"] < 150:
+        price_points = 200
     elif estate["price"] < 200:
-        price_points = 80
+        price_points = 150
     elif estate["price"] < 250:
-        price_points = 60
+        price_points = 100
     elif estate["price"] < 300:
-        price_points = 40
+        price_points = 50
     elif estate["price"] < 350:
-        price_points = 20
+        price_points = 30
     elif estate["price"] < 400:
         price_points = 10
     else:
         price_points = 0
 
-    if estate['distance'] < 1:
+    if estate['distance'] ==  -1:
+        distance_points = 20
+    elif estate['distance'] < 1:
         distance_points = 70
     elif estate['distance'] < 2:
         distance_points = 100
@@ -55,6 +62,7 @@ def calculate_points(estate):
     else:
         distance_points = 0
 
-    return size_points + distance_points + price_points
+    return distance_points + price_points + old
 
 main(name, scrap_urls, ignore_list, calculate_points, distance_from,scrape_file, archive_data_file, print_columns )
+analyze_data(name, ignore_list, calculate_points, distance_from, scrape_file, archive_data_file, print_columns)
