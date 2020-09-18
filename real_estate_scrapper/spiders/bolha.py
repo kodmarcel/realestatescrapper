a7d0e7898e03ae338480b8fc17825bdd908448f7
UnknownHero99 <salmic.jakob@gmail.com>
2020-09-18 12:42:14 +0200

    Fixed bolha scrapper

--- real_estate_scrapper/spiders/bolha.py
+++ real_estate_scrapper/spiders/bolha.py
@@ -1,57 +1,54 @@
-# # -*- coding: utf-8 -*-
-# import scrapy
-# from scrapy import Request
-# from real_estate_scrapper.items import Estate
-# from real_estate_scrapper.itemLoaders import NepremicnineEstateLoader
-# import datetime
-# import csv
-#
-# now = datetime.datetime.now()
-# old_estates_path = "scraped_data/bolha.csv"
-#
-# def get_old_urls(path):
-#     links = []
-#     try:
-#         with open(path, 'r') as infile:
-#             reader = csv.DictReader(infile)
-#             for line in reader:
-#                 links.append(strip_url(line['url']))
-#     except:
-#         pass
-#     return links
-#
-# def strip_url(url):
-#     if 'bolha' in url:
-#         return url.split('.html')[0] + '.html'
-#     return url
-#
-#
-#
-# old_urls = get_old_urls(old_estates_path)
-#
-# class BolhaSpider(scrapy.Spider):
-#     name = 'bolha'
-#     allowed_domains = ['www.bolha.com']
-#     start_urls = ['http://www.bolha.com/nepremicnine/stanovanja/?location=Osrednjeslovenska%2FLjubljana%2F&viewType=30&priceSortField=50000%7C135000&adTypeH=00_Prodam%2F&reSize=43|295']
-#
-#     def parse(self, response):
-#         ads = response.xpath('//div[@class="ad"]')
-#         for ad in ads:
-#             url = strip_url(response.urljoin(ad.xpath('.//a/@href').extract_first()))
-#             if url not in old_urls:
-#                 yield Request(url, callback = self.parse_estate)
-#         follow_url = response.xpath('//a[@class="forward"]/@href').extract_first()
-#         if follow_url:
-#            yield response.follow(follow_url)
-#
-#     def parse_estate(self, response):
-#         loader = NepremicnineEstateLoader(item = Estate(),response = response)
-#         loader.add_xpath('location', '//table[@class="oglas-podatki"]/tr/td[contains(text(),"naselje")]/..//b/text()')
-#         loader.add_xpath('price', '//div[@class="price"]/span/text()')
-#         loader.add_xpath('size', '//table[@class="oglas-podatki"]/tr/td[contains(text(),"Velikost")]/..//b/text()')
-#         loader.add_xpath('floor', '//table[@class="oglas-podatki"]/tr/td[contains(text(),"Nadstropje")]/..//b/text()')
-#         loader.add_xpath('built', '//table[@class="oglas-podatki"]/tr/td[contains(text(),"Leto izgradnje")]/..//b/text()')
-#         loader.add_xpath('text', '//table[@class="main-table"]')
-#         loader.add_value('url',response.url)
-#         loader.add_value('parsed', now.strftime("%d.%m.%Y "))
-#         return loader.load_item()
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Request
+from real_estate_scrapper.items import Estate
+from real_estate_scrapper.itemLoaders import EstateLoader
+import datetime
+import csv
+
+now = datetime.datetime.now()
+pages_limit = "10"
+
+class BolhaSpider(scrapy.Spider):
+    name = 'bolha'
+    allowed_domains = ['www.bolha.com']
+    start_urls = [
+        'http://www.bolha.com/nepremicnine/stanovanja/?location=Osrednjeslovenska%2FLjubljana%2F&viewType=30&priceSortField=50000%7C135000&adTypeH=00_Prodam%2F&reSize=43|295']
+
+    def __init__(self, url = None, scrape_file = None, *args, **kwargs):
+        super(BolhaSpider, self).__init__(*args, **kwargs)
+        self.start_urls = [url]
+        self.scrape_file = scrape_file
+
+    def parse(self, response):
+        ads = response.xpath('//div[contains(@class, "EntityList--Regular")]//li[contains(@class, "EntityList-item")]')
+        for ad in ads:
+            url = response.urljoin(ad.xpath('.//a/@href').extract_first())
+            yield Request(url, callback=self.parse_estate)
+        next_page = response.xpath('//li[contains(@class, "Pagination-item--next")]/button/@data-page').extract_first()
+        if next_page == pages_limit:
+            return
+        if next_page:
+            yield response.follow(response.urljoin("?page=")+next_page)
+
+    def parse_estate(self, response):
+        loader = EstateLoader(item=Estate(), response=response)
+        loader.add_value('page', self.name)
+        loader.add_value('capture_date', now.isoformat())
+        loader.add_value('location', str(response.xpath('//table[contains(@class, "table-summary")]//tr/th[contains(text(),"Lokacija")]/..//td/text()').extract_first()))
+
+        price = response.xpath('//strong[contains(@class, "price")]/text()').extract_first().strip().replace(".", "")
+        if "po dogovoru" in price:
+            price = "-1"
+        loader.add_value('price', str(price))
+
+        size = response.xpath('//table[contains(@class, "table-summary")]//tr/th[contains(text(),"površina")]/..//td/text()').extract_first()
+        if size is None:
+            size = "999"
+        size = size.replace("m²", "").replace(",", ".").strip()
+        loader.add_value('size', str(size))
+        loader.add_xpath('floor', '//table[contains(@class, "table-summary")]//tr/th[contains(text(),"Nadstropje")]/..//td/text()')
+        loader.add_xpath('built', '//table[contains(@class, "table-summary")]//tr/th[contains(text(),"Leto izgradnje")]/..//td/text()')
+        loader.add_xpath('text', '//div[contains(@class, "passage-standard")]//text()')
+        loader.add_value('url', response.url)
+        return loader.load_item()
